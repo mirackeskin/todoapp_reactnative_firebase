fa848b486e86e81ad3e42dbead1e871d34ca008a
mirac <keskinmirac61@gmail.com>
2022-10-02 00:55:30 +0300

    Sign Screen tamamlandı.

--- src/screens/Sign.js
+++ src/screens/Sign.js
@@ -1,7 +1,9 @@
-import { StyleSheet, Text, View,SafeAreaView,TextInput,TouchableOpacity, Alert } from 'react-native'
+import { StyleSheet, Text, View,SafeAreaView,TextInput,TouchableOpacity, Alert,Dimensions } from 'react-native'
 import React, { useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import auth from '@react-native-firebase/auth';
+
+const {width,height}=Dimensions.get("screen");
 
 const Sign = () => {
 
@@ -23,30 +25,37 @@ const Sign = () => {
     }else{
       auth().createUserWithEmailAndPassword(email,password)
       .then(response=>console.log(response))
-      .catch(error=>console.log(error))
+      .catch(error=>{return Alert.alert("Uups!","Bu mail adresi zaten kullanılıyor!",[
+        { text: "Tamam", onPress: () => console.log("OK Pressed") }
+      ])})
     }
   }
 
   return (
     <SafeAreaView style={styles.loginMainWrapper}>
-        <View style={styles.mailWrapper}>
-            <TextInput onChangeText={setEmail} value={email} style={{width:"100%"}} placeholder='E-Mail Adresinizi Girin'></TextInput>
-        </View>
-        <View style={styles.passwordWrapper}>
-            <TextInput onChangeText={setPassword} value={password} placeholder='Şifrenizi Girin'></TextInput>
-        </View>
-        <View style={styles.passwordWrapper}>
-            <TextInput onChangeText={setRepassword} value={repassword} placeholder='Şifrenizi Tekrar Girin'></TextInput>
-        </View>
-        <View style={styles.buttonWrapper}>
-            <TouchableOpacity onPress={signUp} style={{backgroundColor:"#017D72",width:"100%",height:"100%",alignItems:"center",justifyContent:"center",borderRadius:10}}>
-                <Text style={{color:"white"}}>Kayıt Ol</Text>
-            </TouchableOpacity>
-        </View>
-        <View style={styles.buttonWrapper}>
-            <TouchableOpacity onPress={()=>Navigation.goBack()} style={{backgroundColor:"#017D72",width:"100%",height:"100%",alignItems:"center",justifyContent:"center",borderRadius:10}}>
-                <Text style={{color:"white"}}>Geri Dön</Text>
-            </TouchableOpacity>
+      <View style={styles.upBox}>
+        <Text style={{fontSize:width/10,color:"#6001D1"}}>toDaily</Text>
+      </View>
+        <View style={styles.downBox}>
+          <View style={styles.mailWrapper}>
+              <TextInput onChangeText={setEmail} value={email} style={{width:"100%",color:"#6001D1"}} placeholder='E-Mail Adresinizi Girin'  placeholderTextColor={"#6001D1"}></TextInput>
+          </View>
+          <View style={styles.passwordWrapper}>
+              <TextInput onChangeText={setPassword} value={password} style={{width:"100%",color:"#6001D1"}} placeholder='Şifrenizi Girin'  placeholderTextColor={"#6001D1"}></TextInput>
+          </View>
+          <View style={styles.passwordWrapper}>
+              <TextInput onChangeText={setRepassword} value={repassword} style={{width:"100%",color:"#6001D1"}} placeholder='Şifrenizi Tekrar Girin' placeholderTextColor={"#6001D1"}></TextInput>
+          </View>
+          <View style={styles.buttonWrapper}>
+              <TouchableOpacity onPress={signUp} style={{backgroundColor:"#6001D1",width:"100%",height:"100%",alignItems:"center",justifyContent:"center",borderRadius:10,borderWidth:1,borderColor:"white"}}>
+                  <Text style={{color:"white"}}>Kayıt Ol</Text>
+              </TouchableOpacity>
+          </View>
+          <View style={styles.buttonWrapper}>
+              <TouchableOpacity onPress={()=>Navigation.goBack()} style={{backgroundColor:"white",borderWidth:1,borderColor:"#6001D1",width:"100%",height:"100%",alignItems:"center",justifyContent:"center",borderRadius:10}}>
+                  <Text style={{color:"#6001D1"}}>Geri Dön</Text>
+              </TouchableOpacity>
+          </View>
         </View>
     </SafeAreaView>
   )
@@ -57,25 +66,47 @@ export default Sign
 const styles = StyleSheet.create({
   loginMainWrapper:{
     flex:1,
-    backgroundColor:"white",
+    backgroundColor:"#6001D1",
     flexDirection:"column",
     alignItems:"center",
-    justifyContent:"center"
+    justifyContent:"space-evenly"
+},
+upBox:{
+  backgroundColor:"white",
+  borderWidth:2,
+  borderColor:"white",
+  borderBottomLeftRadius:width/10,
+  borderTopRightRadius: width/10,
+  width:width/100*40,
+  height:width/100*20,
+  justifyContent:"center",
+  alignItems:"center" 
+},
+downBox:{
+  backgroundColor:"#6001D1",
+  flexDirection:"column",
+  alignItems:"center",
+  justifyContent:"center",
+  width:"100%"
 },
 mailWrapper:{
-    backgroundColor:"gainsboro",
-    width:"60%",
-    margin:5,
-    borderRadius:10
+  backgroundColor:"white",
+  width:"70%",
+  margin:5,
+  borderRadius:10,
+  borderWidth:3,
+  borderColor:"#6001D1"
 },
 passwordWrapper:{
-    backgroundColor:"gainsboro",
-    width:"60%",
-    margin:5,
-    borderRadius:10
+  backgroundColor:"white",
+  width:"70%",
+  margin:5,
+  borderRadius:10,
+  borderWidth:3,
+  borderColor:"#6001D1"
 },
 buttonWrapper:{
-    width:"60%",
+    width:"70%",
     height:40,
     borderRadius:10,
     margin:5
